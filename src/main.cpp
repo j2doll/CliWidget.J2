031fa27e00b528f116b2848370bb712e2fe06904
Jay Two <j2doll@gmail.com>
2020-12-29 21:52:40 +0900

    updatye example

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ int main() {
     text1.display();
 
     CliWidget::Text text2("Selected value is " + select.getSelectedValue() + "");
+    text2.setForegroundColor(CliWidget::ForegroundColor::BLUE);
     text2.setItalic(true);
     text2.setBlink(true);
     text2.display();
@@ -30,35 +31,36 @@ int main() {
     multiSelect.display();
 
     std::string strText3 = "Selected indexes are ";
-    std::string strText4 = "Selected values are ";
-
     for (int index : multiSelect.getSelectedIndexes()) {
         strText3 += std::to_string(index) + " ";
     }
+    CliWidget::Text text3(strText3);
+    text3.setForegroundColor(CliWidget::ForegroundColor::BLUE);
+    text3.display();
 
+    std::string strText4 = "Selected values are ";
     for (std::string value : multiSelect.getSelectedValues()) {
         strText4 += value + " ";
     }
-
-    CliWidget::Text text3(strText3);
-    text3.display();
-
     CliWidget::Text text4(strText4);
+    text4.setForegroundColor(CliWidget::ForegroundColor::BLUE);
     text4.display();
 
     CliWidget::Text textPwd("Insert a Password:");
+    textPwd.display();
+
     CliWidget::InputPassword inputPsw;
     inputPsw.setRegex("^.{6,}");
-
-    textPwd.display();
     inputPsw.display();
 
     CliWidget::Text textResult("The entered password is " + inputPsw.getValue());
+    textResult.setForegroundColor(CliWidget::ForegroundColor::BLUE);
     textResult.setBold(true);
     textResult.display();
 
     if (!inputPsw.check()) {
         CliWidget::Text textResult("The password is too short.");
+        textResult.setForegroundColor(CliWidget::ForegroundColor::BLUE);
         textResult.display();
     }
 
